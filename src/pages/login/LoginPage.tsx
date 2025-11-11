eabd114ee3d0dd700ac7cdf9bf3947a910b8b0d8
Junseok Ahn <rrvvy9595@gmail.com>
2025-11-11 18:43:38 +0900

    [FIX] 전체적인 위치 변경

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -6,20 +6,22 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="h-full flex flex-col justify-center items-center px-8">
-      <Logo className="w-20 h-10 mb-5" />
-      <h1 className="text-center text-semibold20 font-semibold text-darkgrey05 mb-14">
-        간편하게 로그인하고
-        <br />
-        바로 서비스를 이용해보세요
-      </h1>
-      <button
-        onClick={handleKakaoLogin}
-        className="bg-kakao w-full rounded-md flex items-center justify-center gap-5 py-3 cursor-pointer"
-      >
-        <Kakao className="w-6 h-5" />
-        <span className="text-med16 text-black">카카오계정으로 로그인</span>
-      </button>
+    <div className="relative h-screen">
+      <div className="absolute top-[40%] left-1/2 -translate-1/2 w-full flex flex-col items-center px-8">
+        <Logo className="w-20 h-10 mb-5" />
+        <h1 className="text-center text-semibold20 font-semibold text-darkgrey05 mb-14">
+          간편하게 로그인하고
+          <br />
+          바로 서비스를 이용해보세요
+        </h1>
+        <button
+          onClick={handleKakaoLogin}
+          className="bg-kakao w-full rounded-md flex items-center justify-center gap-5 py-3 cursor-pointer"
+        >
+          <Kakao className="w-6 h-5" />
+          <span className="text-med16 text-black">카카오계정으로 로그인</span>
+        </button>
+      </div>
     </div>
   );
 };
