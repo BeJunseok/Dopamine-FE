5ae4df6f07f38e81cab106ed5c9d60cdb635350e
Junseok Ahn <rrvvy9595@gmail.com>
2025-11-13 17:19:43 +0900

    [FEAT] 포인트 내역 조회 페이지 구현

--- src/pages/my/PointInquiryPage.tsx
+++ src/pages/my/PointInquiryPage.tsx
@@ -1,5 +1,27 @@
+import Header from "@/components/my/header/Header";
+import PointItem from "@/components/my/pointItem/PointItem";
+import { mockPointHistory } from "@/mock/pointHistory";
+
 const PointInquiryPage = () => {
-  return <div>PointInquiry</div>;
+  const pointHistory = mockPointHistory;
+
+  return (
+    <div>
+      <Header />
+
+      <div className="flex flex-col h-full px-4">
+        {pointHistory.map(point => (
+          <PointItem
+            key={point.id}
+            type={point.type}
+            title={point.title}
+            date={point.date}
+            amount={point.amount}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PointInquiryPage;
